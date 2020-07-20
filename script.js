bb0184485aaec4683b477dcf0bc78395a9e78a97
liuzhenxin2 <liuzhenxin1@live.com>
2020-07-20 04:00:24 +0000

    Added script to axios

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 // mainpage video playback speed
 
-document.querySelector("#mainpagevideo").playbackRate = 0.7
+// document.querySelector("#mainpagevideo").playbackRate = 0.7
+
+let apiUrl = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol=msft&apikey=486OHSPRRLZR5IJI';
+
+
+
+
+document.querySelector('#saver-search-btn').addEventListener('click', function(){
+    searchInput = document.querySelector('#saver-search').value;
+    let newApiUrl = `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${searchInput}&apikey=486OHSPRRLZR5IJI
+    `
+    alert(newApiUrl)
+       
+})
+
